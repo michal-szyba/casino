3753064b8e9373c0c01216f64f3a8b3e43a555c8
michal-szyba <mszyba@onet.pl>
2023-06-24 14:59:27 +0200

    Initial commit

---
+++ src/test/java/com/example/gambling/GamblingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gambling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamblingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
